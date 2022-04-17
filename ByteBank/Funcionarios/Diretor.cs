0030aeeec38c691736717bd3acc9e559396ee0db
anoyori <anoyori@gmail.com>
2022-04-16 23:25:56 -0300

    Sobrecarga em POO

---
+++ ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,21 @@
+﻿namespace ByteBank.Funcionarios
+{
+    public class Diretor
+    {
+
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Diretor(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "111.222.333-44";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+        }
+    }
+}
